61d5c895f6b37172c3b6046b5f75f93ac239df11
yunitanurjanah <yunitanurjanah7@gmail.com>
2020-11-04 21:24:39 +0700

    menambahkan component vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 require('./bootstrap');
+
+import VueRouter from 'vue-router'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+window.Vue = require('vue');
+Vue.use(VueRouter)
+
+Vue.component('home-component', require('./components/HomeComponent.vue').default);
+
+// const router = new VueRouter({
+//     mode: 'history',
+//     routes: [
+//         {
+//             path: '/home',
+//             name: 'home',
+//             component: HomeComponent
+//         }
+//     ],
+// });
+
+const app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify()
+});
